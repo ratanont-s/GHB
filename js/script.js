df327799c0b62d7435960e8a38a406c4b7c8228b
unknown <you@example.com>
2018-11-08 03:11:40 +0700

    Update index

--- js/script.js
+++ js/script.js
@@ -14,8 +14,11 @@ $(document).ready(function () {
       }
     }
   });
-  $('[data-toggle="popover"]').popover();
-  $('.list-share li a').popover({
-    placement: 'top'
+  $('a.link-share').popover({
+    template: '<div class="popover popover-social" role="tooltip"><div class="arrow"></div><div class="popover-body p-0"></div></div>',
+    placement: 'top',
+    trigger: 'focus',
+    html: true,
+    content: "<ul class='list-inline'><li class='list-inline-item'>แชร์บอกเพื่อน</li><li class='list-inline-item'><a href='#'><i class='fab fa-facebook'></i></a></li><li class='list-inline-item'><a href='#'><i class='fas fa-envelope'></i></a></li><li class='list-inline-item'><a href='#'><i class='fab fa-line'></i></a></li><li class='list-inline-item'><a href='#'><i class='fab fa-instagram'></i></a></li><li class='list-inline-item'><a href='#'><i class='fab fa-twitter'></i></a></li></ul>"
   });
 });
