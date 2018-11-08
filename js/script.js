bdb67131f8635d3887f6c6efd0fffd5a6e67b49c
Rathanont Sukhapant <rathanont.s@gmail.com>
2018-11-08 18:35:24 +0700

    Update index

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+$(document).ready(function() {
   $(".owl-carousel").owlCarousel({
     loop: true,
     margin: 30,
@@ -14,11 +14,13 @@ $(document).ready(function () {
       }
     }
   });
-  $('a.link-share').popover({
-    template: '<div class="popover popover-social" role="tooltip"><div class="arrow"></div><div class="popover-body p-0"></div></div>',
-    placement: 'top',
-    trigger: 'focus',
+  $("a.link-share").popover({
+    template:
+      '<div class="popover popover-social" role="tooltip"><div class="arrow"></div><div class="popover-body p-0"></div></div>',
+    placement: "top",
+    trigger: "focus",
     html: true,
-    content: "<ul class='list-inline'><li class='list-inline-item'>แชร์บอกเพื่อน</li><li class='list-inline-item'><a href='#'><i class='fab fa-facebook'></i></a></li><li class='list-inline-item'><a href='#'><i class='fas fa-envelope'></i></a></li><li class='list-inline-item'><a href='#'><i class='fab fa-line'></i></a></li><li class='list-inline-item'><a href='#'><i class='fab fa-instagram'></i></a></li><li class='list-inline-item'><a href='#'><i class='fab fa-twitter'></i></a></li></ul>"
+    content:
+      "<ul class='list-inline'><li class='list-inline-item'>แชร์บอกเพื่อน</li><li class='list-inline-item'><a href='#'><i class='fab fa-facebook'></i></a></li><li class='list-inline-item'><a href='#'><i class='fas fa-envelope'></i></a></li><li class='list-inline-item'><a href='#'><i class='fab fa-line'></i></a></li><li class='list-inline-item'><a href='#'><i class='fab fa-instagram'></i></a></li><li class='list-inline-item'><a href='#'><i class='fab fa-twitter'></i></a></li></ul>"
   });
 });
